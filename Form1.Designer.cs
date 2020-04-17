4290f1be38db119ec0ecbee479aa4c0dadc85794
KilroyWasHere-cs-j <59028838+KilroyWasHere-cs-j@users.noreply.github.com>
2020-04-17 10:33:37 -0400

    Add files via upload
    Update - 04/17/2020

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,33 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.UrlTextBox = new System.Windows.Forms.TextBox();
+            this.LoadHtml = new System.Windows.Forms.Button();
+            this.ShowHtmlRawRB = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
             // 
-            // listBox1
+            // UrlTextBox
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(12, 3);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(1610, 433);
-            this.listBox1.TabIndex = 0;
+            this.UrlTextBox.Location = new System.Drawing.Point(12, 12);
+            this.UrlTextBox.Name = "UrlTextBox";
+            this.UrlTextBox.Size = new System.Drawing.Size(586, 20);
+            this.UrlTextBox.TabIndex = 0;
+            // 
+            // LoadHtml
+            // 
+            this.LoadHtml.Location = new System.Drawing.Point(12, 38);
+            this.LoadHtml.Name = "LoadHtml";
+            this.LoadHtml.Size = new System.Drawing.Size(126, 23);
+            this.LoadHtml.TabIndex = 1;
+            this.LoadHtml.Text = "Load Web Page";
+            this.LoadHtml.UseVisualStyleBackColor = true;
+            this.LoadHtml.Click += new System.EventHandler(this.LoadHtml_Click);
+            // 
+            // ShowHtmlRawRB
+            // 
+            this.ShowHtmlRawRB.AutoSize = true;
+            this.ShowHtmlRawRB.Location = new System.Drawing.Point(144, 41);
+            this.ShowHtmlRawRB.Name = "ShowHtmlRawRB";
+            this.ShowHtmlRawRB.Size = new System.Drawing.Size(101, 17);
+            this.ShowHtmlRawRB.TabIndex = 2;
+            this.ShowHtmlRawRB.TabStop = true;
+            this.ShowHtmlRawRB.Text = "Show Raw Html";
+            this.ShowHtmlRawRB.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1663, 450);
-            this.Controls.Add(this.listBox1);
+            this.ClientSize = new System.Drawing.Size(610, 450);
+            this.Controls.Add(this.ShowHtmlRawRB);
+            this.Controls.Add(this.LoadHtml);
+            this.Controls.Add(this.UrlTextBox);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox UrlTextBox;
+        private System.Windows.Forms.Button LoadHtml;
+        private System.Windows.Forms.RadioButton ShowHtmlRawRB;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -9,6 +10,7 @@ namespace WebScraping_wf.cs
 {
     public partial class Form1 : Form
     {
+        public string url = string.Empty;
         public Form1()
         {
             InitializeComponent();
@@ -16,28 +18,89 @@ namespace WebScraping_wf.cs
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            StreamWriter writer = new StreamWriter("C:/TxtFiles/Doc_One.txt");
+
+        }
+
+        private void loadUpHtml()
+        {
+            StreamWriter writer = new StreamWriter("RawHtml.txt");
             writer.Write("");
             writer.Close();
             writer.Dispose();
             WebClient client = new WebClient();
-            client.DownloadFile("https://stackoverflow.com/questions/26233/fastest-c-sharp-code-to-download-a-web-page", "C:/TxtFiles/Doc_One.txt");
-            client.Dispose();
-            scrap();
+            client.DownloadFile(url, "RawHtml.txt");
+            if (ShowHtmlRawRB.Checked == true)
+            {
+                ShowHtmlRawRB.Checked = false;
+                Process.Start("RawHtml.txt");
+                client.Dispose();
+                scrap();
+            }
+            else
+            {
+                client.Dispose();
+                scrap();
+            }
         }
 
         private async void scrap()
         {
-            var url = "https://www.ebay.com/sch/i.html?_nkw=Chemical+Suits&_in_kw=1&_ex_kw=&_sacat=0&_udlo=&_udhi=&_ftrt=901&_ftrv=1&_sabdlo=&_sabdhi=&_samilow=&_samihi=&_sadis=15&_stpos=&_sargn=-1%26saslc%3D1&_salic=1&_sop=12&_dmd=1&_ipg=50&_fosrp=1";
-            var httpClient = new HttpClient();
-            var html = await httpClient.GetStringAsync(url);
-            var htmlDocument = new HtmlAgilityPack.HtmlDocument();
-            htmlDocument.LoadHtml(html);
+            //<summary>
+            //Scraps the webpage for the requested data
+            //<summary>
+            var httpClient = new HttpClient(); //creates httpClient
+            var html = await httpClient.GetStringAsync(url); //not sure what the await is doing
+            var htmlDocument = new HtmlAgilityPack.HtmlDocument(); //creates the htmmlDocument
+            htmlDocument.LoadHtml(html); //loads htmlDoc
 
-            var ProductList = htmlDocument.DocumentNode.Descendants("ul").Where(node => node.GetAttributeValue("id", "").Equals("ListViewInner")).ToList();
-            foreach(var item in ProductList)
+            var ProductList = htmlDocument.DocumentNode.Descendants(queryConfig("Descendants - ").ToString()).Where(node => node.GetAttributeValue(queryConfig("Attribute - ").ToString(), "").Equals(queryConfig("ListViewInner - ").ToString().ToList())); //gets the right data
+            foreach (var item in ProductList)
             {
-                listBox1.Items.Add(item.InnerHtml);  
+                postProcess(item.InnerHtml, item.InnerLength, item.InnerText, item.InnerStartIndex); //pass over the data it finds to the 
+            }
+        }
+
+        private void postProcess(string InnerHtml, int InnerLenght, string InnerText, int StartIndex)
+        {
+            //<summary>
+            //will take care of cleaning up the above strings 
+            //InnerText is the string you want Gabby
+            //<summary>
+        }
+
+        public string queryConfig(string idOfItem)
+        {
+            //<summary>
+            //gets data from config file [idOfItem] is a header string to id a config item
+            //<summary>
+            string item = String.Empty;
+            StreamReader reader = new StreamReader("Form1FunctionConfig.txt");
+            var lines = reader.ReadToEnd().Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            for(int i = 0; i != lines.Length; i++)
+            {
+                if (lines[i].Contains(idOfItem))
+                {
+                    item = lines[i].Replace(idOfItem, ""); //replaces idOfItem so it's ownly the item itself
+                    return item;
+                }
+            }
+            return $"No item with the give ID [ {idOfItem} ] was found"; //if it can't find the item
+        }
+
+        private void LoadHtml_Click(object sender, EventArgs e)
+        {
+            //<summary>
+            //load the html from the web page into a txt file for later refernce
+            //also insures that the URL input text box has something in it 
+            //<summary>
+            if(UrlTextBox.Text == string.Empty)
+            {
+                MessageBox.Show("URLError", "Please Enter vaild URL");
+            }
+            else
+            {
+                url = UrlTextBox.Text;
+                loadUpHtml();
             }
         }
     }
